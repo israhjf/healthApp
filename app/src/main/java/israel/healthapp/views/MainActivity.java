57695fa74730cc05e43c157d9be49180ab36d075
israhjf <hjfisrael@gmail.com>
2021-03-08 11:41:07 -0700

    Creat project, work on onboarding pages

---
+++ app/src/main/java/israel/healthapp/views/MainActivity.java
@@ -0,0 +1,16 @@
+package israel.healthapp.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import israel.healthapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
