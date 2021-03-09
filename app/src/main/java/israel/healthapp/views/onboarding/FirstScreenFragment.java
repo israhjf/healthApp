0b67e1e3d4819d20065b324c98e857ada9027724
israhjf <hjfisrael@gmail.com>
2021-03-09 04:23:33 -0700

    Created Onboarding screens

--- app/src/main/java/israel/healthapp/views/onboarding/FirstScreenFragment.java
+++ app/src/main/java/israel/healthapp/views/onboarding/FirstScreenFragment.java
@@ -10,22 +10,13 @@ import android.view.ViewGroup;
 
 import israel.healthapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * create an instance of this fragment.
- */
-public class viewPagerFragment extends Fragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-    }
+public class FirstScreenFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_pager, container, false);
+        return inflater.inflate(R.layout.fragment_first_screen, container, false);
     }
 }
---
+++ app/src/main/java/israel/healthapp/views/onboarding/ThirdScreenFragment.java
@@ -0,0 +1,21 @@
+package israel.healthapp.views.onboarding;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import israel.healthapp.R;
+
+public class ThirdScreenFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third_screen, container, false);
+    }
+}
