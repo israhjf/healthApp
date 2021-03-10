9cb7fb2ed1e8b51cc3ed3183d7b8197f73714883
israhjf <hjfisrael@gmail.com>
2021-03-10 08:10:46 -0700

    Deleted SplashScreen

--- app/src/main/java/israel/healthapp/views/MainActivity.java
+++ app/src/main/java/israel/healthapp/views/MainActivity.java
@@ -1,18 +1,29 @@
 package israel.healthapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 import israel.healthapp.R;
+import israel.healthapp.views.onboarding.ViewPagerAdapter;
+import israel.healthapp.views.onboarding.ViewPagerFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         getSupportActionBar().hide();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, ViewPagerFragment.class, null)
+                    .commit();
+        }
     }
 }
